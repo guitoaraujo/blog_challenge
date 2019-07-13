5569b832c3a210d0b6d1051eba205b2f3ad31114
igor araujo <igor.araujo@fulllab.com.br>
2019-07-13 13:58:03 -0300

    added pagination using kaminari and kaminari theme bootstrap4

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'rails_admin'
 gem 'devise'
+gem 'kaminari'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :verify_user!, except: [:index, :new, :create]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc).page params[:page]
   end
 
   def show
